a67f0fa33858008b9edd3fb5f477f54d1f1db276
Nick Carneiro <burthawk101@gmail.com>
2014-01-03 19:34:45 -0600

    added highcharts line chart

---
+++ app/scripts/highcharts-line.js
@@ -0,0 +1,120 @@
+$(function () {
+
+    // Register a parser for the American date format used by Google
+    Highcharts.Data.prototype.dateFormats['m/d/Y'] = {
+        regex: '^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{2})$',
+        parser: function (match) {
+            return Date.UTC(+('20' + match[3]), match[1] - 1, +match[2]);
+        }
+    };
+
+    // Get the CSV and create the chart
+    $.get('/data/aapl.csv', function (csv) {
+
+        $('#line-chart-container').highcharts({
+
+            data: {
+                csv: csv
+            },
+
+            title: {
+                text: 'AAPL Daily Close'
+            },
+
+            subtitle: {
+                text: 'Source: Yahoo Finance'
+            },
+
+            xAxis: {
+                type: 'datetime',
+                tickInterval: 7 * 24 * 3600 * 1000, // one week
+                tickWidth: 0,
+                gridLineWidth: 1,
+                labels: {
+                    align: 'left',
+                    x: 3,
+                    y: -3
+                }
+            },
+
+            yAxis: [{ // left y axis
+                title: {
+                    text: null
+                },
+                labels: {
+                    align: 'left',
+                    x: 3,
+                    y: 16,
+                    formatter: function() {
+                        return Highcharts.numberFormat(this.value, 0);
+                    }
+                },
+                showFirstLabel: false
+            }, { // right y axis
+                linkedTo: 0,
+                gridLineWidth: 0,
+                opposite: true,
+                title: {
+                    text: null
+                },
+                labels: {
+                    align: 'right',
+                    x: -3,
+                    y: 16,
+                    formatter: function() {
+                        return Highcharts.numberFormat(this.value, 0);
+                    }
+                },
+                showFirstLabel: false
+            }],
+
+            legend: {
+                align: 'left',
+                verticalAlign: 'top',
+                y: 20,
+                floating: true,
+                borderWidth: 0
+            },
+
+            tooltip: {
+                shared: true,
+                crosshairs: true
+            },
+
+            plotOptions: {
+                series: {
+                    cursor: 'pointer',
+                    point: {
+                        events: {
+                            click: function() {
+                                hs.htmlExpand(null, {
+                                    pageOrigin: {
+                                        x: this.pageX,
+                                        y: this.pageY
+                                    },
+                                    headingText: this.series.name,
+                                    maincontentText: Highcharts.dateFormat('%A, %b %e, %Y', this.x) +':<br/> '+
+                                        this.y +' visits',
+                                    width: 200
+                                });
+                            }
+                        }
+                    },
+                    marker: {
+                        lineWidth: 1
+                    }
+                }
+            },
+
+            series: [{
+                name: 'Price',
+                lineWidth: 4,
+                marker: {
+                    radius: 4
+                }
+            }]
+        });
+    });
+
+});
+
