2b08dc5cec983dcb37f9f2311f9ccf64d4c05b8b
Nick Carneiro <burthawk101@gmail.com>
2014-01-04 16:52:00 -0600

    got line chart working for nvd3

---
+++ app/scripts/nvd3-line.js
@@ -0,0 +1,44 @@
+
+$.get('/data/aapl.json', function(data) {
+    function exampleData() {
+        return [
+
+            {
+                "key" : "Price" ,
+                "values" : data
+            }
+        ].map(function(series) {
+                series.values = series.values.map(function(d) { return {x: d[0], y: d[1] } });
+                return series;
+            });
+    }
+
+    nv.addGraph(function() {
+        var testdata = exampleData(),
+            chart = nv.models.lineChart()
+                .margin({top: 30, right: 60, bottom: 50, left: 70})
+                .x(function(d,i) { return i })
+                .color(d3.scale.category10().range());
+
+        chart.xAxis.tickFormat(function(d) {
+            var dx = testdata[0].values[d] && testdata[0].values[d].x || 0;
+            return d3.time.format('%x')(new Date(dx))
+        });
+
+        chart.yAxis
+                 .tickFormat(d3.format(',f'));
+
+        //chart.lines.forceY([0]);
+
+        d3.select('#line-chart-container svg')
+            .datum(exampleData())
+            .transition().duration(500).call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+    });
+
+});
+
+
